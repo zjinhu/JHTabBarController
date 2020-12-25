e640b16df728ac6bde93dce949e7be5f5027618c
Jackie <jackie>
2020-12-25 16:28:40 +0800

    no message

--- Sources/JHTabBarController/JHTabBar.swift
+++ Sources/JHTabBarController/JHTabBar.swift
@@ -9,13 +9,20 @@
 import UIKit
 import SnapKit
 public class JHTabBar: UITabBar {
-
+    
     private var buttons: [JHTabBarButton] = []
-
+    
     private var itemFrames = [CGRect]()
     private var tabBarItems = [UIView]()
     
-     public override var selectedItem: UITabBarItem? {
+//    private lazy var indicator: UIView = {
+//        let circle = UIView(frame: .zero)
+//        circle.backgroundColor = tintColor
+//        addSubview(circle)
+//        return circle
+//    }()
+    
+    public override var selectedItem: UITabBarItem? {
         willSet {
             guard let newValue = newValue else {
                 buttons.forEach { (item) in
@@ -24,13 +31,14 @@ public class JHTabBar: UITabBar {
                 return
             }
             guard let index = items?.firstIndex(of: newValue),
-                index != NSNotFound else {
-                    return
+                  index != NSNotFound else {
+                return
             }
             select(itemAt: index, animated: false)
+//            animate(index: index)
         }
     }
-        
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -48,12 +56,12 @@ public class JHTabBar: UITabBar {
     }()
     
     private func configure() {
-///12.0 12.1 tabbar偏移修复,系统bug
-//        if #available(iOS 12.2, *) {
-//
-//        } else {
-//            isTranslucent = false
-//        }
+        ///12.0 12.1 tabbar偏移修复,系统bug
+        //        if #available(iOS 12.2, *) {
+        //
+        //        } else {
+        //            isTranslucent = false
+        //        }
         addSubview(container)
         let bottomOffset: CGFloat
         if #available(iOS 11.0, *) {
@@ -67,7 +75,6 @@ public class JHTabBar: UITabBar {
             make.right.equalToSuperview().offset(-10)
             make.bottom.equalToSuperview().offset(-bottomOffset)
         }
-
     }
     
     override public func safeAreaInsetsDidChange() {
@@ -120,7 +127,7 @@ public class JHTabBar: UITabBar {
             }
             lastButton = button
         }
-
+        
         layoutIfNeeded()
     }
     
@@ -135,9 +142,9 @@ public class JHTabBar: UITabBar {
     
     @objc private func btnPressed(sender: JHTabBarButton) {
         guard let index = buttons.firstIndex(of: sender),
-            index != NSNotFound,
-            let item = items?[index] else {
-                return
+              index != NSNotFound,
+              let item = items?[index] else {
+            return
         }
         buttons.forEach { (button) in
             guard button != sender else {
@@ -146,7 +153,7 @@ public class JHTabBar: UITabBar {
             button.deselect()
         }
         sender.select()
-
+        
         delegate?.tabBar?(self, didSelect: item)
     }
     
@@ -164,5 +171,22 @@ public class JHTabBar: UITabBar {
         selectedbutton.select(animated: false)
     }
     
-
+//    private func animate(index: Int) {
+//
+//        guard index < buttons.count else {
+//            return
+//        }
+//        let selectedbutton = buttons[index]
+//
+//        indicator.snp.remakeConstraints { (make) in
+//            make.centerX.bottom.equalTo(selectedbutton)
+//            make.width.height.equalTo(4)
+//        }
+//
+//        let animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeInOut) {
+//            self.layoutIfNeeded()
+//        }
+//
+//        animator.startAnimation()
+//    }
 }
